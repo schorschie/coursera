80e286c7f2d60ac73741e8bbaffb132f89acb916
Grzegorz Lippe (AS/ENS-W) <Grzegorz.Lippe@bosch.com>
2022-08-22 16:48:00 +0200

    Restore own partitioning souveniers solution

--- algorythmic_toolbox/course_material/week6_dynamic_programming2/2_partitioning_souvenirs/partition3.py
+++ algorythmic_toolbox/course_material/week6_dynamic_programming2/2_partitioning_souvenirs/partition3.py
@@ -29,30 +29,30 @@ def optimal_weight(W, w):
     
     return optimal_path
 
-def get_is_partitionable(A):
-    
-    W = sum(A) // 3
-    n = len(A)
-    items = A.copy()
-
-    count = 0 
-    value = np.zeros((W+1, n+1))
-
-    for i in range(1, W+1):
-        for j in range(1, n+1):
-            value[i][j] = value[i][j-1]
-            if items[j-1]<=i:
-                temp = value[i-items[j-1]][j-1] + items[j-1]
-                if temp > value[i][j]:
-                    value[i][j] = temp
-            if value[i][j] == W:
-                count += 1
-
-
-    if count < 3:
-        return 0
-    else: 
-        return 1
+# def get_is_partitionable(A):
+#     
+#     W = sum(A) // 3
+#     n = len(A)
+#     items = A.copy()
+# 
+#     count = 0 
+#     value = np.zeros((W+1, n+1))
+# 
+#     for i in range(1, W+1):
+#         for j in range(1, n+1):
+#             value[i][j] = value[i][j-1]
+#             if items[j-1]<=i:
+#                 temp = value[i-items[j-1]][j-1] + items[j-1]
+#                 if temp > value[i][j]:
+#                     value[i][j] = temp
+#             if value[i][j] == W:
+#                 count += 1
+# 
+# 
+#     if count < 3:
+#         return 0
+#     else: 
+#         return 1
     
 def partition3(A):
     if any([a <= 0 for a in A]):
@@ -60,13 +60,13 @@ def partition3(A):
 
     is_partitionable = False
     if sum(A) % 3 == 0 and len(A) >= 3 and sum(A) >= 3:
-        #a = sum(A) // 3
-        #op = optimal_weight(a, A)
+        a = sum(A) // 3
+        op = optimal_weight(a, A)
         
         # For the souvenirs to be partitionable into 3 subsets, there must
         # be at least three sub-knapsacks with the value of a:
-        # is_partitionable = sum([sum([cell == a for cell in row]) for row in op]) >= 3
-        is_partitionable = get_is_partitionable(A)
+        is_partitionable = sum([sum([cell == a for cell in row]) for row in op]) >= 3
+        # is_partitionable = get_is_partitionable(A)
 
     return int(is_partitionable)
 
